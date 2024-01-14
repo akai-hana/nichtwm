0cd4c1950cd86f498dbeaa952c9f556d70da6de1
Matias Moya <matiasmoya1701@proton.me>
2024-01-14 15:26:47 +0100

    add stuff lmao

---
+++ nichtwm.cc
@@ -0,0 +1,52 @@
+// struct that holds compositor state
+struct nichtwm {
+    struct wl_display *wl_display;
+    struct wl_event_loop *wl_event_loop;
+};
+
+// wayland display event loop
+// (app signals, notification of data file descriptors, etc)
+int main(int argc, char **argv) {
+    struct nichtwm server;
+
+    server.wl_display = wl_display_create();
+    assert(server.wl_display);
+    server.wl_event_loop = wl_display_get_event_loop(server.wl_display);
+    assert(server.wl_event_loop);
+
+    // wlroots helper function
+    // (auto. chooses best backend based on users environment)
+    server.backend = wlr_backend_autocreate(server.wl_display);
+    assert(server.backend);
+    return 0;
+};
+
+// wlroots backend
+// abstracts low level in/output implementations
+// (mice, keyboards, monitors, etc)
+
+/*
+Available backends:
+    - drm: render to displays
+    - libinput: enumerates/controls physical input
+    - wayland: outputs as windows on another wayland compositor.
+               allows to nest compositors (debugging)
+    - x11: same as wayland but using x11.
+    - multi: combine multiple backends and their in/outputs.
+*/
+
+struct nichtwm {
+    struct wl_display *wl_display;
+    struct wl_event_loop *wl_event_loop;
+
+    struct wlr_backend *backend;
+};
+
+// start backend and enter Wayland event loop
+if (!wlr_backend_start(server.backend)) {
+    fprintf(stderr, "Failed to start backend\n");
+    wl_display_destroy(server.wl_display);
+    return 1;
+};
+
+return 0;
