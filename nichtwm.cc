b6105a0bc97e006b7a0bba2717541319619fa7b1
Matias Moya <55887501+akai-hana@users.noreply.github.com>
2024-01-24 10:43:23 +0100

    tatakaee

--- nichtwm.cc
+++ nichtwm.cc
@@ -1,21 +1,53 @@
 #include <wayland-client.h>
 
 // struct that holds compositor state
-struct nichtwm {
+struct nichtwm_server {
     struct wl_display *wl_display;
     struct wl_event_loop *wl_event_loop;
 
+    // wlroots backend
     struct wlr_backend *backend; // include backend member
-
+    // listens for new outputs
     struct wl_listener new_output;
 
+    // abstracts low level in/output implementations
+    // (mice, keyboards, monitors, etc)
     struct wl_list outputs; // nichtwm_output::link
 };
+
+struct nichtwm_output {
+    struct wlr_output *wlr_output;
+    struct nichtwm_server *server;
+    struct timespec last_frame;
+
+    struct wl_list link;
+};
+
+static void new_output_notify(struct wl_listener *listener, void *data) {
+    struct nichtwm_server *server = wl_container_of(listener, server, new_output);
+    
+    struct wlr_output *wlr_output = data;
+
+    if (!wl_list_empty(&wlr_output->modes)) {
+        struct wlr_output_mode *mode = wl_container_of(wlr_output->modes.prev, mode, link);
+        wlr_output_set_mode(wlr_output, mode);
+    }
+    
+    // Call nwm output function
+    struct nichtwm_output *output = calloc(1, sizeof(struct nichtwm_output));
+    clock_gettime(CLOCK_MONOTONIC, &output->last_frame);
+    output->server = server;
+    output->wlr_output = wlr_output;
+    wl_list_insert(&server->outputs, &output->link);
+}
+
+
+//  structure we use to store any state we have for this output that is specific to our compositor’s needs
 
 // wayland display event loop
 // (app signals, notification of data file descriptors, etc)
 int main(int argc, char **argv) {
-    struct nichtwm server;
+    struct nichtwm_server server;
 
     // create wayland display
     server.wl_display = wl_display_create();
@@ -30,6 +62,11 @@ int main(int argc, char **argv) {
     server.backend = wlr_backend_autocreate(server.wl_display);
     assert(server.backend);
 
+    // specify function to be notified, via wl_signal_add
+    wl_list_init(&server.outputs);
+    server.new_output.notify = new_output_notify;
+    wl_signal_add(&server.backend->events.new_output, &server.new_output);
+
     // start backend and enter Wayland event loop
     if (!wlr_backend_start(server.backend)) {
         fprintf(stderr, "Failed to start backend\n");
@@ -40,10 +77,6 @@ int main(int argc, char **argv) {
     return 0;
 };
 
-// wlroots backend
-// abstracts low level in/output implementations
-// (mice, keyboards, monitors, etc)
-
 /*
 Available backends:
     - drm: render to displays
@@ -53,4 +86,3 @@ Available backends:
     - x11: same as wayland but using x11.
     - multi: combine multiple backends and their in/outputs.
 */
-
